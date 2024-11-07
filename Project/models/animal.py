251f2a83230e186b2e7f9fc32dcbdeef982885d2
zepaulojr1 <64874053+zepaulojr1@users.noreply.github.com>
2024-11-07 12:04:50 -0700

    added lab_id to animal class

--- Project/models/animal.py
+++ Project/models/animal.py
@@ -1,6 +1,7 @@
 class Animal:
-    def __init__(self, animal_id, name, initial_weight, last_weight, last_weighted):
-        self.animal_id = animal_id
+    def __init__(self, animal_id, lab_animal_id, name, initial_weight, last_weight, last_weighted):
+        self.animal_id = animal_id  # Database-generated ID
+        self.lab_animal_id = lab_animal_id  # User-defined lab animal ID
         self.name = name
         self.initial_weight = initial_weight
         self.last_weight = last_weight
@@ -9,6 +10,7 @@ class Animal:
     def to_dict(self):
         return {
             'animal_id': self.animal_id,
+            'lab_animal_id': self.lab_animal_id,
             'name': self.name,
             'initial_weight': self.initial_weight,
             'last_weight': self.last_weight,
@@ -19,6 +21,7 @@ class Animal:
     def from_dict(data):
         return Animal(
             animal_id=data['animal_id'],
+            lab_animal_id=data['lab_animal_id'],
             name=data['name'],
             initial_weight=data['initial_weight'],
             last_weight=data['last_weight'],
