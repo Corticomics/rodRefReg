2c94ee823fe8d6b62f07e02cb164bc6b07f03fab
zepaulojr1 <64874053+zepaulojr1@users.noreply.github.com>
2024-11-07 15:49:10 -0700

    Update login_system.py

--- Project/models/login_system.py
+++ Project/models/login_system.py
@@ -22,7 +22,7 @@ class LoginSystem:
             trainer = self.database_handler.get_trainer_by_id(trainer_id)
             if trainer:
                 self.current_trainer = trainer
-                print(f"Trainer '{trainer['trainer_name']}' logged in successfully.")
+                print(f"Trainer '{trainer['username']}' logged in successfully.")
                 return True
             else:
                 print(f"Login failed: Invalid trainer data received. Data: {trainer}")
